f3ed47a1de9aae1b761c4ed127d3e43c1e8d604b
Vadim Manaenko <vadim.razorq@gmail.com>
2018-06-27 11:44:08 +0300

    Fix error and CMakeFile

---
+++ setup.py
@@ -0,0 +1,12 @@
+# DO NOT USE
+# python setup.py install
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['zenit_printer_agent'],
+    package_dir={'': 'src'})
+setup(**setup_args)
+
--- src/zenit_printer_agent/printagent.py
+++ src/zenit_printer_agent/printagent.py
@@ -9,11 +9,11 @@ class PrintAgent:
     def __init__(self):
         rospy.init_node('printagent_node')
 
-        rospy.Subscriber('/path_to_gcode', String, self.print)
+        rospy.Subscriber('/path_to_gcode', String, self.print_gcode)
 
-    def print(data):
+    def print_gcode(data):
         rospy.loginfo('Got a task to print {}'.format(data.data))
-'''
+        '''
         req = urllib2.Request('http://[fced:97fd:da23:b15d:7897:4fa6:57b3:f2a3]/api/files/local/' + data.data)
         req.add_header('Content-Type', 'application/json')
         req.add_header('X-API-KEY', apikey.apikey())
@@ -25,8 +25,7 @@ class PrintAgent:
 
         response = urllib2.urlopen(req, json.dumps(data))
 
-'''
-
+        '''
         req = urllib2.Request('http://[fced:97fd:da23:b15d:7897:4fa6:57b3:f2a3]/api/files/local/ok?recursive=true')
         req.add_header('X-API-KEY', apikey.apikey())
         response = urllib2.urlopen(req)
